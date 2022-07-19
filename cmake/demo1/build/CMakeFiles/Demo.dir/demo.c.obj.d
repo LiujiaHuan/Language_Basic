870413a505e37a378087e2dde973e86f58920293
LiujiaHuan <1395622672@qq.com>
2022-07-19 16:27:41 +0800

    Update CMake

---
+++ cmake/demo1/build/CMakeFiles/Demo.dir/demo.c.obj.d
@@ -0,0 +1,14 @@
+CMakeFiles/Demo.dir/demo.c.obj: C:\Users\13956\Desktop\666\demo.c \
+ F:/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ F:/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ F:/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ F:/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ F:/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ F:/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ F:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ F:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ F:/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ F:/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ F:/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ F:/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ F:/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h
